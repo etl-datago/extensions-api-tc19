457f22d3d65930e418b7f95159d6722519d7d485
Ashwin Sekar <asekar@tableau.com>
2019-11-08 11:25:33 -0800

    change html

--- demo-snippets/0-SETUP.js
+++ demo-snippets/0-SETUP.js
@@ -2,21 +2,22 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- These two libraries are local versions of datatables: https://datatables.net/download/ -->
-  <link rel="stylesheet" type="text/css" href="/lib/datatables.min.css" />
-  <script type="text/javascript" src="/lib/datatables.min.js"></script>
+    <!-- These two libraries are local versions of datatables: https://datatables.net/download/ -->
+    <link rel="stylesheet" type="text/css" href="/lib/datatables.min.css"/>
+    <script type="text/javascript" src="/lib/datatables.min.js"></script>
+    
+    <!-- Include the Tableau Extensions library so we can communicate with Tableau -->
+    <script src="/lib/tableau.extensions.1.latest.js"></script>
 
-  <!-- Include the Tableau Extensions library so we can communicate with Tableau -->
-  <script src="/lib/tableau.extensions.1.latest.js"></script>
-
-  <!-- Include our own style sheets and javascript code -->
-  <link rel="stylesheet" href="./Tutorial.css">
-  <script src="./uiHelpers.js"></script>
-  <script src="./superstore.js"></script>
+    <!-- Include our own style sheets and javascript code -->
+    <link rel="stylesheet" href="../../demo-snippets/complete.css">
+    <link rel="stylesheet" href="../../lib/google-material-icons.css">
+    <script src="../../lib/demoHelpers.js"></script>
+    <script src="./superstore.js"></script>
 </head>
 <style>
   .center {
