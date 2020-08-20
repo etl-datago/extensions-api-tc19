613bcd08f9a368db7e7663661768cd03cf4f59c6
etl-datago <etl@datago.com.br>
2020-08-20 09:20:45 -0300

    Funcional

--- Samples/Superstore/superstore.js
+++ Samples/Superstore/superstore.js
@@ -3,15 +3,52 @@
 // Wrap everything in an anonymous function to avoid poluting the global namespace
 (function() {
 
-  // Use the jQuery document ready signal to know when everything has been initialized
+  // Quando o documento HTML é carregado. Carregue:
   $(document).ready(function() {
     tableau.extensions.initializeAsync({'configure': showChooseSheetDialog}).then(function() {
-      $("#test").append("I have initialized!");
+      // $("#test").append("I have initialized!");   // Funçao de inicializar
+      showChooseSheetDialog();
     });
     
     function showChooseSheetDialog() {
-      $("#test").append("<br>Choose Sheet");
+      //obtem nome do Dashboard
+      const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
+
+      //obtem vetor do nome das planilhas
+      const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+      
+      //trata para pegar apenas os dashboards
+      const worksheetNames = worksheets.map((worksheet) => {
+        return worksheet.name;
+      });
+      
+      //resposta
+      demoHelpers.showDialog(dashboardName, worksheetNames, loadSelectedMarks);
     }
+
+
+
+    function loadSelectedMarks(worksheetName) {
+      //Texto para omarcas selecionada
+      $('#selected_marks_title').text(worksheetName);
+
+      //Obtem nome marcas selecionada
+      const worksheet = demoHelpers.getSelectedSheet(worksheetName);
+      
+      // Obtem todas as marcas selecionadas
+      worksheet.getSelectedMarksAsync().then((marks) => {
+          //Popular datatable
+          demoHelpers.populateDataTable(marks);
+      });
+    }
+
+
+
+
+
+
+
+    
 
   });
 })();
