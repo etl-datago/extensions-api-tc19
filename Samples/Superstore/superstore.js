1dd84a93da6e78fe220f1e07ecdb88503ec12935
etl-datago <etl@datago.com.br>
2020-08-20 10:26:45 -0300

    Complete v1

--- Samples/Superstore/superstore.js
+++ Samples/Superstore/superstore.js
@@ -3,11 +3,27 @@
 // Wrap everything in an anonymous function to avoid poluting the global namespace
 (function() {
 
+  let removeEventListener;
+
+  let filteredColumns = [];
+
   // Quando o documento HTML é carregado. Carregue:
   $(document).ready(function() {
     tableau.extensions.initializeAsync({'configure': showChooseSheetDialog}).then(function() {
       // $("#test").append("I have initialized!");   // Funçao de inicializar
-      showChooseSheetDialog();
+      //showChooseSheetDialog();
+      
+      //Ação do botão
+      $('#reset_filters_button').click(resetFilters);
+      $('#show_choose_sheet_button').click(showChooseSheetDialog);
+    
+      const savedSheetName = tableau.extensions.settings.get('sheet');
+      if (savedSheetName) {
+        loadSelectedMarks(savedSheetName);
+      } else {
+        showChooseSheetDialog();
+      }
+
     });
     
     function showChooseSheetDialog() {
@@ -23,11 +39,15 @@
       });
       
       //resposta
-      demoHelpers.showDialog(dashboardName, worksheetNames, loadSelectedMarks);
+      demoHelpers.showDialog(dashboardName, worksheetNames, saveSheetAndLoadSelectedMarks);
     }
 
     // carregar marcados
     function loadSelectedMarks(worksheetName) {
+      if (removeEventListener) {
+        removeEventListener();
+      }
+
       //Texto para omarcas selecionada
       $('#selected_marks_title').text(worksheetName);
 
@@ -37,11 +57,49 @@
       // Obtem todas as marcas selecionadas
       worksheet.getSelectedMarksAsync().then((marks) => {
           //Popular datatable
-          demoHelpers.populateDataTable(marks);
+          demoHelpers.populateDataTable(marks, filterByColumn);
       });
+      
+      // obtem marcas selecionadas assincronas      
+      const marksSelectedEventHandler = (event) => {
+        loadSelectedMarks(worksheetName);
+      }
+      removeEventListener = worksheet.addEventListener(
+        tableau.TableauEventType.MarkSelectionChanged, marksSelectedEventHandler);
+
     }
 
+    function saveSheetAndLoadSelectedMarks(worksheetName) {
+      tableau.extensions.settings.set('sheet', worksheetName);
+      tableau.extensions.settings.saveAsync();
+      loadSelectedMarks(worksheetName);
+    }
+
+    // ao clicar na coluna, ira filtrar dados por coluna
+    function filterByColumn(columnIndex, fieldName) {
+      //obtem valores da coluna
+      const columnValues = demoHelpers.getValuesInColumn(columnIndex);
+      const worksheet = demoHelpers.getSelectedSheet(tableau.extensions.settings.get('sheet'));
+
+      worksheet.applyFilterAsync(fieldName, columnValues, tableau.FilterUpdateType.Replace);
+
+      filteredColumns.push(fieldName);
+  }
+
+
+
+  function resetFilters() {
+    const worksheet = demoHelpers.getSelectedSheet(tableau.extensions.settings.get('sheet'));
+    filteredColumns.forEach((columnName) => {
+        worksheet.clearFilterAsync(columnName);
+    });
+
+    filteredColumns = [];
+  }
+
+
+
+
+
   });
-
-
 })();
